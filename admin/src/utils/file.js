276b945815312599225683fbb529d2286057ca73
thanhtran <thanhtran@thanhtrans-iMac-Pro.local>
2022-02-15 21:47:17 +0700

    add departure_date

--- admin/src/utils/file.js
+++ admin/src/utils/file.js
@@ -50,6 +50,7 @@ export const readLocalFile = (file) => {
               schedule_file: element.CHUONG_TRINH_TOUR || "",
               price_and_include_file: element.GIA_VA_BAO_GOM || "",
               to_destination: element.DIEM_DEN || "",
+              departure_date: element.NGAY_KHOI_HANH || "",
             });
           } else {
             console.log(`tour_id ${tourId} has exist`);
