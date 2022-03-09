d036d9b47aa6940b03c900b9cef9c3c07327ed2a
Thanh Tran <tranthanh.it.95@gmail.com>
2022-03-09 17:01:20 +0700

    add get national destionals

--- admin/src/utils/contentApis.js
+++ admin/src/utils/contentApis.js
@@ -1,19 +1,40 @@
-import { request } from 'strapi-helper-plugin';
-import { filter } from 'lodash';
-import pluralize from 'pluralize';
-import { MODEL_KIND } from '../constants/model-kind';
+import { request } from "strapi-helper-plugin";
+import { filter } from "lodash";
+import pluralize from "pluralize";
+import { MODEL_KIND } from "../constants/model-kind";
 
 export const getModels = () => {
-  return request('/content-type-builder/content-types', {
-    method: 'GET',
-  }).then((response) => {
-    return filter(response.data, (model) => !model.plugin);
-  }).catch(() => {
-    return [];
-  });
+  return request("/content-type-builder/content-types", {
+    method: "GET",
+  })
+    .then((response) => {
+      return filter(response.data, (model) => !model.plugin);
+    })
+    .catch(() => {
+      return [];
+    });
+};
+
+export const getNationalDestinations = () => {
+  return request(
+    "/content-manager/collection-types/application::national-destination.national-destination?page=1&pageSize=100&_sort=name:ASC",
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => {
+      return filter(response.data.results, true).map((obj) => ({
+        id: obj.id,
+        name: obj.name,
+      }));
+    })
+    .catch(() => {
+      return [];
+    });
 };
 
 export const fetchEntries = (apiId, kind) => {
-  const url = (kind === MODEL_KIND.collection) ? `/${pluralize(apiId)}` : `/${apiId}`;
-  return request(url, { method: 'GET' });
+  const url =
+    kind === MODEL_KIND.collection ? `/${pluralize(apiId)}` : `/${apiId}`;
+  return request(url, { method: "GET" });
 };
--- admin/src/utils/file.js
+++ admin/src/utils/file.js
@@ -1,5 +1,7 @@
 import xlsx from "xlsx";
 
+import { getNationalDestinations } from "./contentApis";
+
 export const readLocalFile = (file) => {
   const result = [];
   return new Promise((resolve, reject) => {
@@ -10,6 +12,7 @@ export const readLocalFile = (file) => {
       const data = event.target.result;
       const workbook = xlsx.read(data, { type: "binary" });
       console.log("debug 01");
+      const nationalDestinations = await getNationalDestinations();
       workbook.SheetNames.forEach((sheet) => {
         console.log("inside foreach");
         const rowObject = xlsx.utils.sheet_to_json(workbook.Sheets[sheet]);
@@ -32,6 +35,8 @@ export const readLocalFile = (file) => {
             }
           }
           const tourId = parseInt(element.TOUR_ID);
+          const nationalDestination = findNameInArray(element.DIEM_KHOI_HANH, nationalDestinations);
+          console.log('nationalDestination', nationalDestination);
           if (tourId > 0 && !mapTourId.has(tourId)) {
             result.push({
               title: element.TEN_TOUR || "",
@@ -57,47 +62,13 @@ export const readLocalFile = (file) => {
             console.log(`tour_id ${tourId} has exist`);
           }
         }
-
-        // const rowObject = xlsx.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-        // if (rowObject && !rowObject.length) {
-        //   return result;
-        // }
-
-        // rowObject.map(item => {
-        //   console.log('item', item);
-        //   let images = [];
-        //   if (item.ANH_GALLERY.length > 0) {
-        //     const imagesGallery = item.ANH_GALLERY.split(',');
-        //     for (let index = 0; index < imagesGallery.length; index++) {
-        //       const element = imagesGallery[index];
-        //       images.push(`https://fiditour.com${element}`)
-        //     }
-        //   }
-        //   const tourId = parseInt(item.TOUR_ID);
-        //   if (tourId > 0 && !mapTourId.has(tourId)) {
-        //     result.push({
-        //       title: item.TEN_TOUR || '',
-        //       tour_id: tourId || 0,
-        //       thumbnail_url: item.ANH_THUMB || '',
-        //       price: parseInt(item.GIA_TOUR.replace(',', '')) || 0,
-        //       time_travel: item.THOI_GIAN_DI || '',
-        //       tour_code: tourId,
-        //       number_of_seats: 0,
-        //       vehicle: item.PHUONG_TIEN || '',
-        //       overview_text: item.NOIDUNG_NGAN || '',
-        //       list_image_url: JSON.parse(JSON.stringify(Object.assign({}, images))),
-        //       rules_file: item.DIEU_KHOAN || '',
-        //       schedule_file: item.CHUONG_TRINH_TOUR || '',
-        //       price_and_include_file: item.GIA_VA_BAO_GOM || '',
-        //       to_destination: item.DIEM_DEN || '',
-        //     });
-        //   } else {
-        //     console.log(`tour_id ${tourId} has exist`);
-        //   }
-        // });
       });
       resolve(result);
     };
     fileReader.onerror = reject;
   });
 };
+
+const findNameInArray = (name, arr) => {
+  return arr.find(data => data.name == name);
+}
