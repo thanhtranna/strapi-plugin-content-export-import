2bcdf2e4732906cd6d928a6c857d2c53c2c9a140
Thanh Tran <tranthanh.it.95@gmail.com>
2022-03-09 18:06:50 +0700

    add logs

--- admin/src/utils/file.js
+++ admin/src/utils/file.js
@@ -15,7 +15,7 @@ export const readLocalFile = (file) => {
       getNationalDestinations()
         .then((nationalDestinations) => {
           workbook.SheetNames.forEach((sheet) => {
-            console.log("inside foreach", JSON.stringify(nationalDestinations));
+            console.log("inside foreach");
             const rowObject = xlsx.utils.sheet_to_json(workbook.Sheets[sheet]);
             if (rowObject && !rowObject.length) {
               console.log("rowObject length = 0");
@@ -41,29 +41,32 @@ export const readLocalFile = (file) => {
                 nationalDestinations
               );
               console.log("nationalDestination", nationalDestination);
-              if (tourId > 0 && !mapTourId.has(tourId)) {
-                result.push({
-                  title: element.TEN_TOUR || "",
-                  tour_id: tourId || 0,
-                  thumbnail_url: element.ANH_THUMB || "",
-                  price: parseInt(element.GIA_TOUR.replace(",", "")) || 0,
-                  time_travel: element.THOI_GIAN_DI || "",
-                  tour_code: tourId,
-                  number_of_seats: 0,
-                  vehicle: element.PHUONG_TIEN || "",
-                  overview_text: element.NOIDUNG_NGAN || "",
-                  list_image_url: JSON.parse(
-                    JSON.stringify(Object.assign({}, images))
-                  ),
-                  rules_file: element.DIEU_KHOAN || "",
-                  schedule_file: element.CHUONG_TRINH_TOUR || "",
-                  price_and_include_file: element.GIA_VA_BAO_GOM || "",
-                  from_destination: element.DIEM_KHOI_HANH || "",
-                  to_destination_name: element.DIEM_DEN || "",
-                  departure_date: element.NGAY_KHOI_HANH || "",
-                });
-              } else {
-                console.log(`tour_id ${tourId} has exist`);
+              if (nationalDestination) {
+                if (tourId > 0 && !mapTourId.has(tourId)) {
+                  result.push({
+                    title: element.TEN_TOUR || "",
+                    tour_id: tourId || 0,
+                    thumbnail_url: element.ANH_THUMB || "",
+                    price: parseInt(element.GIA_TOUR.replace(",", "")) || 0,
+                    time_travel: element.THOI_GIAN_DI || "",
+                    tour_code: tourId,
+                    number_of_seats: 0,
+                    vehicle: element.PHUONG_TIEN || "",
+                    overview_text: element.NOIDUNG_NGAN || "",
+                    list_image_url: JSON.parse(
+                      JSON.stringify(Object.assign({}, images))
+                    ),
+                    rules_file: element.DIEU_KHOAN || "",
+                    schedule_file: element.CHUONG_TRINH_TOUR || "",
+                    to_destination: nationalDestination.id,
+                    price_and_include_file: element.GIA_VA_BAO_GOM || "",
+                    from_destination: element.DIEM_KHOI_HANH || "",
+                    to_destination_name: element.DIEM_DEN || "",
+                    departure_date: element.NGAY_KHOI_HANH || "",
+                  });
+                } else {
+                  console.log(`tour_id ${tourId} has exist`);
+                }
               }
             }
           });
